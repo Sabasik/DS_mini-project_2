140225234cf44dc5b345f1870c563279a379d017
Henrik Lepson <henrikk895@gmail.com>
2023-05-01 15:54:27 +0300

    create processes command

--- node.py
+++ node.py
@@ -42,10 +42,63 @@ class Node:
         return f'Node: {self.name}, address: {self.address}, online: {self.online}'
     
 
+class Process:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.predecessor = None
+        self.successor = None
+        self.store = {}
+        self.head = False
+        self.tail = False
+
+    def set_predecessor(self, process):
+        self.predecessor = process
+
+    def set_successor(self, process):
+        self.successor = process
+
+    def set_head(self, head):
+        self.head = head
+
+    def set_tail(self, tail):
+        self.tail = tail    
+
+
 class ChainServicer(chain_pb2_grpc.ChainServicer):
     def __init__(self, name, address) -> None:
         self.name = name
         self.address = address
+        self.server = None
+        self.processes = {}
+
+    def set_server(self, server):
+        self.server = server
+
+    def create_process(self):
+        name = f'{self.name}-PS{len(self.processes.keys()) + 1}'
+        process = Process(name)
+        self.processes[name] = process
+        print(f'Created process {name}')
+
+    def process_command(self, command: str):
+        if command.startswith('Local-store-ps'):
+            k = int(command.split(' ')[1])
+            self.local_store_ps(k)
+        elif command == 'exit':
+            print('Should exit the server here m8')
+            #stop_server()
+        else:
+            print('Unsupported command')
+
+    def local_store_ps(self, k: int):
+        '''
+        k: the number of storages to create
+        '''
+        for i in range(k):
+            self.create_process()
+
+    def stop_server(self):
+        self.server.stop(0)
 
     def Probe(self, request, context):
         return chain_pb2.ProbeResponse(name=self.name)
@@ -101,10 +154,17 @@ def serve():
     chain_pb2_grpc.add_ChainServicer_to_server(servicer, server)
     server.add_insecure_port(args.addr)
     server.start()
+    servicer.set_server(server)
 
     print(f'Started node: {args.name} on {args.addr}')
-
     wait_for_nodes()
+
+    try:
+        while True:
+            user_command = input(f'{args.name}>')
+            servicer.process_command(user_command)
+    except KeyboardInterrupt:
+        server.stop(0)
 
         
 if __name__ == '__main__':
