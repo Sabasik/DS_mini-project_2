9f06b151648ed5389c8d76afe19ace0e30167d6d
Kadi Sammul <kadi.sammul@ut.ee>
2023-05-02 10:31:56 +0300

    format fixes

--- node.py
+++ node.py
@@ -14,6 +14,7 @@ required.add_argument('-addr', type=str, help='IP address of the created node in
 parser.add_argument('-file', type=str, default='node_list.txt', help='Path to the node list file')
 args = parser.parse_args()
 
+
 class Node:
     def __init__(self, address) -> None:
         self.name = None
@@ -39,7 +40,7 @@ class Node:
         except:
             print(f'Node at {self.address} is not online yet')
             return None
-        
+
     def get_processes(self):
         '''
         Get the list of node processes
@@ -52,7 +53,7 @@ class Node:
         except:
             print(f'Failed to get processes of node {self.name}({self.address})')
             return None
-        
+
     def send_chain(self, chain):
         '''
         Set the chain order (list of processes in the correct order) for other node
@@ -65,7 +66,7 @@ class Node:
         except:
             print(f'Failed to send chain to node {self.name}({self.address})')
             return None
-        
+
     def send_book_data(self, target_process, book, price):
         try:
             with grpc.insecure_channel(self.address) as channel:
@@ -75,7 +76,7 @@ class Node:
         except:
             print(f'Failed to send book data to node {self.name}({self.address})')
             return None
-        
+
     def get_book_list(self, target_process):
         try:
             with grpc.insecure_channel(self.address) as channel:
@@ -87,7 +88,7 @@ class Node:
         except:
             print(f'Failed to get book list from node {self.name}({self.address})')
             return None
-        
+
     def get_book_price(self, process, book):
         try:
             with grpc.insecure_channel(self.address) as channel:
@@ -100,7 +101,7 @@ class Node:
 
     def __repr__(self) -> str:
         return f'Node: {self.name}, address: {self.address}, online: {self.online}'
-    
+
 
 class Process:
     def __init__(self, name) -> None:
@@ -126,7 +127,7 @@ class Process:
     def store_book(self, book, price):
         book_data = {}
         book_data['price'] = price
-        book_data['dirty'] = None # Placeholder for a future task
+        book_data['dirty'] = None  # Placeholder for a future task
         self.store[book] = book_data
 
     def get_books(self):
@@ -135,7 +136,7 @@ class Process:
             listing.append(f'{key} = {self.store[key]["price"]}')
 
         return listing
-    
+
     def get_book_price(self, book):
         try:
             book = self.store[book]
@@ -173,7 +174,7 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
             if node.name == target_node_name:
                 target_node = node
                 break
-        
+
         return target_node
 
     def process_order(self):
@@ -199,17 +200,17 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
         for key in list(self.processes.keys()):
             process = self.processes[key]
             index = self.chain_order.index(process.name)
-            #Head
+            # Head
             if index == 0:
                 process.set_head(True)
                 process.set_successor(self.chain_order[index + 1])
 
-            #Tail
+            # Tail
             elif index == len(self.chain_order) - 1:
                 process.set_tail(True)
                 process.set_predecessor(self.chain_order[index - 1])
 
-            #Normal
+            # Normal
             else:
                 process.set_predecessor(self.chain_order[index - 1])
                 process.set_successor(self.chain_order[index + 1])
@@ -253,7 +254,7 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
         if len(self.chain_order) == 0:
             print('Chain has not been created yet')
             return
-        
+
         for i, process in enumerate(self.chain_order):
             if i == 0:
                 print(f'{process} (Head)', end=' -> ')
@@ -313,7 +314,7 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
             self.list_books()
         elif command.startswith('Read-operation'):
             book = command.replace('Read-operation', '').strip()
-            book = book[1:len(book) - 1] # Also removing quotes
+            book = book[1:len(book) - 1]  # Also removing quotes
             self.read_operation(book)
         elif command == 'exit':
             self.stop_server()
@@ -333,11 +334,11 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
     # grpc implementations
     def Probe(self, request, context):
         return chain_pb2.ProbeResponse(name=self.name)
-        
+
     def Processes(self, request, context):
         processes = list(self.processes.keys())
         return chain_pb2.ProcessResponse(processes=processes)
-    
+
     def SetChain(self, request, context):
         print('Received chain ordering')
         chain_order = request.processes
@@ -347,7 +348,7 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
             self.processes[key].reset()
         self.update_processes()
         return chain_pb2.ChainResponse()
-    
+
     def get_target_process(self, target_name):
         target_process = None
         for key in list(self.processes.keys()):
@@ -357,26 +358,26 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
                 break
 
         return target_process
-    
+
     def SendBook(self, request, context):
         target_process = self.get_target_process(request.process)
 
         book = request.book
         price = request.price
         target_process.store_book(book, price)
-        # If the chain continues send the book to the next member
+        # If the chain continues, send the book to the next member
         if target_process.successor is not None:
             target_node = self.get_process_node(target_process.successor)
             thread = Thread(target=target_node.send_book_data, args=(target_process.successor, book, price,))
             thread.start()
 
         return chain_pb2.SendBookResponse()
-    
+
     def ListBooks(self, request, context):
         target_process = self.get_target_process(request.process)
         books = target_process.get_books()
         return chain_pb2.ListBooksResponse(books=books)
-    
+
     def GetBookPrice(self, request, context):
         # Get the book from the required process
         target_process = self.get_target_process(request.process)
@@ -392,7 +393,6 @@ class ChainServicer(chain_pb2_grpc.ChainServicer):
         # TODO: show that the price has actually changed
         price = head_price
         return chain_pb2.BookResponse(price=price)
-        
 
 
 # Reads the node file and returns an array of Node objects
@@ -415,7 +415,7 @@ def check_node_statuses(nodes):
             node.online = True
             continue
 
-        if node.online == True:
+        if node.online:
             print(f'Node at {node.address} is online')
             continue
 
@@ -460,6 +460,6 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
 
-        
+
 if __name__ == '__main__':
     serve()
